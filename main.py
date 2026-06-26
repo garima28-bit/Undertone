aa7660382970100b75ba050b44057205035dfb24
itsveshesh <vesheshgaur2007@gmail.com>
2026-06-26 23:02:11 +0530

    Added test pics and code to get RGB vals

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("hi")
+from PIL import Image
+
+img = Image.open("test_pics/testpic_1.jpeg")
+img = img.convert("RGB")
+img = img.resize((10, 10))
+img.show()
+img.save("test_pics/testpic_1_resized.png")
+
+red = []
+green = []
+blue = []
+
+for i in range(0, 10):
+    for j in range(0, 10):
+        r, g, b = img.getpixel((i, j))
+        red.append(r)
+        green.append(g)
+        blue.append(b)
+
+print(red, len(red))
+print(green, len(green))
+print(blue, len(blue))
